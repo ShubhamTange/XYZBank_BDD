5597ab415e221875cd5ad6d2780b6384a53a56f2
shubham <shubham@example.com>
2025-07-17 14:30:25 +0530

    Added All changes

---
+++ src/test/java/stepdefinations/TransactionsTC.java
@@ -0,0 +1,23 @@
+package stepdefinations;
+
+import base.TestBase;
+import io.cucumber.java.en.Then;
+
+public class TransactionsTC extends TestBase{
+	
+	@Then("user clicks on transactionsOption")
+	public void user_clicks_on_transactions_option() {
+	    // Write code here that turns the phrase above into concrete actions
+	    System.out.println("Pending Implementation");
+	}
+	@Then("validate the transaction page and validate the url")
+	public void validate_the_transaction_page_and_validate_the_url() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("Pending Implementation");
+	}
+	@Then("validate the table details #needs to add map to validate the details")
+	public void validate_the_table_details_needs_to_add_map_to_validate_the_details() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("Pending Implementation");
+	}
+}
